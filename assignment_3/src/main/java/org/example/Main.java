183d02dcd86fae7b4c44eb585510b48ce28c4e30
IsFariza <fariza.ismag@gmail.com>
2025-10-26 17:22:40 +0500

    feat(main): add main

---
+++ assignment_3/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.OutputHandler.JsonOutputWriter;
+
+
+public class Main {
+    public static void main(String[] args) {
+        JsonOutputWriter.jsonWriteOutput();
+    }
+}
